8af9bd0d76936eb45fd7897f70c06adc887a25f2
DivyaVajja <33012526+DivyaVajja@users.noreply.github.com>
2017-11-04 17:22:29 -0400

    Add files via upload

---
+++ src/viz1.js
@@ -0,0 +1,17 @@
++import React, { Component } from 'react';
++import logo from './logo.svg';
++import './styles/home-style.css';
++import {Button, Navbar, Dr} from 'react-bootstrap/lib';
++import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const element = (
+  <div>
+
+
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
